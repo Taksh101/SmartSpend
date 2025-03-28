e0ed3f6a9b2e635c33d895f03e330eed1f4483b4
Taksh Bhatt <138237522+Taksh101@users.noreply.github.com>
2025-03-28 20:12:31 +0530

    Update settings.py

--- smartspend/settings.py
+++ smartspend/settings.py
@@ -116,6 +116,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://smartspend-production.up.railway.app"
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
