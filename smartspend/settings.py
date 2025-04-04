23326d57ab0bd7136144d5930f6d7b203e3deb65
Taksh Bhatt <takshbhatt0512@gmail.com>
2025-04-04 19:14:52 +0530

    Bugs Fixed

--- smartspend/settings.py
+++ smartspend/settings.py
@@ -122,7 +122,7 @@ CSRF_TRUSTED_ORIGINS = [
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
